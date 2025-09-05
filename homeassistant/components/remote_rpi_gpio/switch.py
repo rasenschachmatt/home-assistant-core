981c2d887ceea1d224d7ce3da5e203ba7d226322
rasenschachmatt <rasenschachmatt@gmail.com>
2025-09-05 23:07:47 +0200

    Add unique id for remote_rpi_gpio switch

--- homeassistant/components/remote_rpi_gpio/switch.py
+++ homeassistant/components/remote_rpi_gpio/switch.py
@@ -17,7 +17,13 @@ from homeassistant.helpers import config_validation as cv
 from homeassistant.helpers.entity_platform import AddEntitiesCallback
 from homeassistant.helpers.typing import ConfigType, DiscoveryInfoType
 
-from . import CONF_INVERT_LOGIC, DEFAULT_INVERT_LOGIC, setup_output, write_output
+from . import (
+    CONF_INVERT_LOGIC,
+    DEFAULT_INVERT_LOGIC,
+    DOMAIN,
+    setup_output,
+    write_output,
+)
 
 CONF_PORTS = "ports"
 
@@ -39,17 +45,17 @@ def setup_platform(
     discovery_info: DiscoveryInfoType | None = None,
 ) -> None:
     """Set up the Remote Raspberry PI GPIO devices."""
-    address = config[CONF_HOST]
+    host = config[CONF_HOST]
     invert_logic = config[CONF_INVERT_LOGIC]
     ports = config[CONF_PORTS]
 
     devices = []
     for port, name in ports.items():
         try:
-            led = setup_output(address, port, invert_logic)
+            led = setup_output(host, port, invert_logic)
         except (ValueError, IndexError, KeyError, OSError):
             return
-        new_switch = RemoteRPiGPIOSwitch(name, led)
+        new_switch = RemoteRPiGPIOSwitch(name, led, host, port)
         devices.append(new_switch)
 
     add_entities(devices)
@@ -61,11 +67,19 @@ class RemoteRPiGPIOSwitch(SwitchEntity):
     _attr_assumed_state = True
     _attr_should_poll = False
 
-    def __init__(self, name: str | None, led: LED) -> None:
+    def __init__(self, name: str | None, led: LED, host: str, port: int) -> None:
         """Initialize the pin."""
         self._attr_name = name or DEVICE_DEFAULT_NAME
         self._attr_is_on = False
         self._switch = led
+        self._host = host
+        self._port = port
+
+    @property
+    def unique_id(self) -> str:
+        """Return a unique ID based on host and port."""
+        host_string = self._host.replace(".", "_").replace(":", "_")
+        return f"{DOMAIN}_{host_string}_{self._port}"
 
     def turn_on(self, **kwargs: Any) -> None:
         """Turn the device on."""
